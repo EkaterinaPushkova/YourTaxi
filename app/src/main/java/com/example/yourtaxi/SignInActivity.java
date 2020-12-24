76a785c0489334b35f33b7ec20bb9515e41eda9a
Ekaterina <pushkova1666@gmail.com>
2020-12-24 16:55:39 +0300

    finish0

--- app/src/main/java/com/example/yourtaxi/SignInActivity.java
+++ app/src/main/java/com/example/yourtaxi/SignInActivity.java
@@ -1,23 +1,36 @@
 package com.example.yourtaxi;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class SignInActivity extends AppCompatActivity{
+public class SignInActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sign_in);
     }
-    public void toMap (View view) {
-        Intent i = new Intent( SignInActivity.this, MapsActivity.class);
+
+    public void toMap(View view) {
+        Intent i = new Intent(SignInActivity.this, MapsActivity.class);
         startActivity(i);
     }
-    public void toSU (View view) {
-        Intent i = new Intent( SignInActivity.this, SignUpActivity.class);
+
+    public void toSU(View view) {
+        Intent i = new Intent(SignInActivity.this, SignUpActivity.class);
         startActivity(i);
+    }
+
+    public void goToWeb(View view) {
+        goToUrl("http://google.com");
+    }
+
+    private void goToUrl(String url) {
+        Uri uriUrl = Uri.parse(url);
+        Intent launchBrowser = new Intent(Intent.ACTION_VIEW, uriUrl);
+        startActivity(launchBrowser);
     }
 }
