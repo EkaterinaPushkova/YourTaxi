f85011e9bf18138d63947f985ede6991911c0474
Ekaterina <pushkova1666@gmail.com>
2020-12-24 11:57:26 +0300

    activities

--- app/src/main/java/com/example/yourtaxi/HistoryActivity.java
+++ app/src/main/java/com/example/yourtaxi/HistoryActivity.java
@@ -1,15 +1,17 @@
 package com.example.yourtaxi;
 
+import androidx.appcompat.R;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class HistoryActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.history);
     }
+
 }
--- app/src/main/java/com/example/yourtaxi/SignInActivity.java
+++ app/src/main/java/com/example/yourtaxi/SignInActivity.java
@@ -1,6 +1,9 @@
 package com.example.yourtaxi;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SignInActivity extends AppCompatActivity{
@@ -8,5 +11,9 @@ public class SignInActivity extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sign_in);
+    }
+    public void toMap (View view) {
+        Intent i = new Intent(startActivity(nav_main );
+        finish();
     }
 }
